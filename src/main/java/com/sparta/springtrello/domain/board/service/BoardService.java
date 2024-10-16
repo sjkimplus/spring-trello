6176085c0856419630e4c722198c62d24d547719
dhPark <pdh6113@naver.com>
2024-10-17 02:51:23 +0900

    [Refactor] (Board) validation 수정

--- src/main/java/com/sparta/springtrello/domain/board/service/BoardService.java
+++ src/main/java/com/sparta/springtrello/domain/board/service/BoardService.java
@@ -10,6 +10,7 @@ import com.sparta.springtrello.domain.kanban.dto.response.KanbanResponseDto;
 import com.sparta.springtrello.domain.kanban.entity.Kanban;
 import com.sparta.springtrello.domain.kanban.repository.KanbanRepository;
 import com.sparta.springtrello.domain.member.entity.Member;
+import com.sparta.springtrello.domain.member.entity.MemberRole;
 import com.sparta.springtrello.domain.member.repository.MemberRepository;
 import com.sparta.springtrello.domain.ticket.dto.TicketResponseDto;
 import com.sparta.springtrello.domain.ticket.entity.Ticket;
@@ -40,15 +41,18 @@ public class BoardService {
     @Transactional
     public BoardResponseDto createBoard(AuthUser authUser, Long workspaceId, String title, String background, String image) {
 
-        validation(authUser);
+        //유저의 멤버 정보 가져오기
+        Member member = memberRepository.findByWorkspaceIdAndUserId(workspaceId,authUser.getId())
+                .orElseThrow(()-> new HotSixException(ErrorCode.USER_NOT_FOUND));
+
+        //Board를 등록하려는 유저의 role이 reader인지 확인
+        if (member.getMemberRole().equals(MemberRole.ROLE_READER)){
+            throw new RuntimeException();
+        }
 
         //Board를 등록할 워크스페이스 찾기
         Workspace workspace = workspaceRepository.findById(workspaceId).orElseThrow(() ->
                 new HotSixException(ErrorCode.WORKSPACE_NOT_FOUND));
-
-        //Board 관리하는 Member 찾기
-        Member member = memberRepository.findById(authUser.getId()).orElseThrow(() ->
-                new HotSixException(ErrorCode.MEMBER_NOT_FOUND));
 
         //Board 생성
         Board board = new Board(member, workspace, title, background, image);
@@ -85,11 +89,18 @@ public class BoardService {
     @Transactional
     public BoardResponseDto updateBoard(Long id, AuthUser authUser, String title, String background, String image) {
 
-        validation(authUser);
-
         //수정하려는 Board 찾기
         Board board = boardRepository.findById(id).orElseThrow(() ->
                 new HotSixException(ErrorCode.BOARD_NOT_FOUND));
+
+        //Board를 등록하는 멤버 찾기
+        Member member = memberRepository.findByWorkspaceIdAndUserId(board.getWorkspace().getId(),authUser.getId())
+                .orElseThrow(()-> new HotSixException(ErrorCode.USER_NOT_FOUND));
+
+        //Board를 등록하려는 유저의 role이 reader인지 확인
+        if (member.getMemberRole().equals(MemberRole.ROLE_READER)){
+            throw new RuntimeException();
+        }
 
         //Board 수정
         board.updateBoard(title, background, image);
@@ -101,28 +112,23 @@ public class BoardService {
     @Transactional
     public void deleteBoard(Long id, AuthUser authUser) {
 
-        validation(authUser);
-
         //삭제하려는 Board 찾기
         Board board = boardRepository.findById(id).orElseThrow(() ->
                 new HotSixException(ErrorCode.BOARD_NOT_FOUND));
+
+        //Board를 등록하는 멤버 찾기
+        Member member = memberRepository.findByWorkspaceIdAndUserId(board.getWorkspace().getId(),authUser.getId())
+                .orElseThrow(()-> new HotSixException(ErrorCode.USER_NOT_FOUND));
+
+        //Board를 등록하려는 유저의 role이 reader인지 확인
+        if (member.getMemberRole().equals(MemberRole.ROLE_READER)){
+            throw new RuntimeException();
+        }
 
         //Board soft-delete
         board.deleteBoard();
 
         //Board 하위계층인 kanban, ticket, comment의 soft-delete 연동
 
-    }
-
-    private void validation(AuthUser authUser) {
-        //탈퇴한 유저인지 확인
-        userService.checkUser(authUser.getId());
-
-        //유저가 Board가 속한 Workspace의 Member인지 확인
-        Member member = memberRepository.findById(authUser.getId()).orElseThrow(() ->
-                new HotSixException(ErrorCode.MEMBER_NOT_FOUND));
-
-        //유저의 권한 확인
-        if (member.getMemberRole().toString().equals("ROLE_READER")) throw new HotSixException(ErrorCode.USER_NO_AUTHORITY);
     }
 }
