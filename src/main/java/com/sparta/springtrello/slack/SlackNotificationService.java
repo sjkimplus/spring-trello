79af855e34ca30fd6770cf64b921df803fe88d8f
checkuu-0216 <corb37@gmail.com>
2024-10-17 20:53:12 +0900

    [Fix] (alarm) 잠시 주석

--- src/main/java/com/sparta/springtrello/slack/SlackNotificationService.java
+++ src/main/java/com/sparta/springtrello/slack/SlackNotificationService.java
@@ -11,27 +11,27 @@ import org.springframework.web.client.RestTemplate;
 import java.util.HashMap;
 import java.util.Map;
 
-@Service
-public class SlackNotificationService {
-
-    @Value("${slack.webhook.url}")
-    private String slackWebhookUrl;
-
-    private final RestTemplate restTemplate = new RestTemplate();
-
-    public void sendSlackMessage(String message) {
-        // Header 생성
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-
-        // 메세지 생성
-        Map<String, String> body = new HashMap<>();
-        body.put("text", message);
-
-        // Map
-        HttpEntity<Map<String, String>> request = new HttpEntity<>(body, headers);
-
-        // Slack webhook URL으로 POST 요청 보내기
-        restTemplate.exchange(slackWebhookUrl, HttpMethod.POST, request, String.class);
-    }
-}
+//@Service
+//public class SlackNotificationService {
+//
+//    @Value("${slack.webhook.url}")
+//    private String slackWebhookUrl;
+//
+//    private final RestTemplate restTemplate = new RestTemplate();
+//
+//    public void sendSlackMessage(String message) {
+//        // Header 생성
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.setContentType(MediaType.APPLICATION_JSON);
+//
+//        // 메세지 생성
+//        Map<String, String> body = new HashMap<>();
+//        body.put("text", message);
+//
+//        // Map
+//        HttpEntity<Map<String, String>> request = new HttpEntity<>(body, headers);
+//
+//        // Slack webhook URL으로 POST 요청 보내기
+//        restTemplate.exchange(slackWebhookUrl, HttpMethod.POST, request, String.class);
+//    }
+//}
